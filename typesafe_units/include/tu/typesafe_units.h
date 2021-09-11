70a546360620ec253a8017a481687aaa8c79d4a5
Klas Marcks <klas.marcks@gmail.com>
2021-09-11 13:34:20 +0200

    Remove redundant empty line

--- typesafe_units/include/tu/typesafe_units.h
+++ typesafe_units/include/tu/typesafe_units.h
@@ -509,7 +509,6 @@ struct second_squared : Coherent_unit<s<(TU_TYPE)2.0>, m<(TU_TYPE)0.0>, kg<(TU_T
 struct metre_cubed: Coherent_unit<s<(TU_TYPE)0.0>, m<(TU_TYPE)3.0>, kg<(TU_TYPE)0.0>, A<(TU_TYPE)0.0>, K<(TU_TYPE)0.0>, mol<(TU_TYPE)0.0>, cd<(TU_TYPE)0.0>>{};
 struct metre_squared: Coherent_unit<s<(TU_TYPE)0.0>, m<(TU_TYPE)2.0>, kg<(TU_TYPE)0.0>, A<(TU_TYPE)0.0>, K<(TU_TYPE)0.0>, mol<(TU_TYPE)0.0>, cd<(TU_TYPE)0.0>>{};
 
-
 //
 // Define non coherent units
 //
